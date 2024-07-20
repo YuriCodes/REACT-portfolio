79b06a6f189d5667cd878ab984a20e1ed3d98d6b
Yuri <yuricodesbot@gmail.com>
2024-07-20 15:48:37 -0400

    fixed alt

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -3,7 +3,7 @@ function Form(){
         <div id="Contact">
         <h2 className="big-headers contact-header">Let's talk </h2>
         <p className='contact-text'>I want to hear from you. I'm open to any ideas or collaborations, <br />
-        So don't hesitate to <a href="https://linktr.ee/YuriCodesBot" target="_blank" rel="noreferrer" >reach out!</a> </p> 
+        So don't hesitate to <a href="https://linktr.ee/YuriCodesBot" target="_blank" rel="noreferrer" alt="reach out" >reach out!</a> </p> 
            
         </div>
     )
--- src/WritingCard.jsx
+++ src/WritingCard.jsx
@@ -3,7 +3,7 @@ function WritingCard(props){
     return(
         <div id="writing" className="box-shadow writing-box" >
             <h3 className='header-txt deep-blue work-title'>
-             <a href={props.href} target="_blank" rel="noreferrer" alt={props.alt} >{props.title} </a>
+             <a href={props.href} target="_blank" rel="noreferrer" alt="title">{props.title} </a>
             </h3>
             <a href={props.href} target="_blank" rel="noreferrer" alt="linktomore" ></a>
             <p className='work-description'>{props.description} </p>
